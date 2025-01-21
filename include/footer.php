f12e173ef050061df84f0d3a9e827167a9b1352d
Dulshan Rajapaksha <r.m.p.n.dulshanrajapaksha@gmail.com>
2025-01-21 22:23:36 +0530

    header and footer created

---
+++ include/footer.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- custom css -->
+    <link rel="stylesheet" href="./css/footer-style.css">
+
+</head>
+<body class="">
+
+
+    <!-- footer start -->
+
+    <div class="bg-light-purple">
+        <div class="container">
+            <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+              <div class="col mb-3">
+                <img src="../assets/Footer/footer-icon.png" alt="Online Galaxy" style="height: 200px;">
+                <p class="text-body-secondary">© 2024</p>
+              </div>
+          
+              <div class="col mb-3">
+                <h2 class="fw-bold fs-3">Online Galaxy</h2>
+                <h5>Got Question</h5>
+                <div class="lh-1 f">
+                    <p class="lead">Call us from 8 a.m.  - 8 p.m.</p>
+                    <p class="lead">+94 77 50 50 505</p>
+                    <p class="lead">Kingdom  Streets,</p>
+                    <p class="lead">Yahapahuwa Junction,</p>
+                    <p class="lead">Maho.</p>
+                </div>
+    
+    
+              </div>
+          
+              <div class="col mb-3">
+                <h5 class="text-decoration-underline lea">Customer Care</h5>
+                <ul class="nav flex-column">
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Corporate Inquiry</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Return & Refund</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Services</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Payments</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">CRM</a></li>
+                </ul>
+              </div>
+          
+              <div class="col mb-3">
+                <h5 class="text-decoration-underline">Get Know to US</h5>
+                <ul class="nav flex-column">
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About Us</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Contact Us</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Address</a></li>
+                </ul>
+              </div>
+          
+              <div class="col mb-3">
+                <h5 class="text-decoration-underline">Lets US  help YOU</h5>
+                <ul class="nav flex-column">
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">My Account</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">My Orders</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Terms & Conditions</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li>
+                  <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Privacy Policy</a></li>
+                </ul>
+              </div>
+            </footer>
+        </div>
+
+
+        <!-- copyright text start -->
+
+        <div class="d-flex flex-column flex-sm-row justify-content-center py-4 my-4 border-top bg-dark-purple">
+            <p class="text-white">© 2024 Company, Inc. All rights reserved.</p>
+        </div>
+
+        <!-- copyright end -->
+
+
+    </div>
+
+    <!-- footer end -->
+
+
+    
+
+    <!-- Bootstrap js -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+</body>
+</html>
